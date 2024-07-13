56c0b3f92f78643b1a9952f3370dcb36f573cc38
cpart700 <cathexxan2@gmail.com>
2024-07-13 11:02:22 +0800

    message

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>First React App Installation</h1>
+        <h5>What is React?</h5>
+        <p>
+        React is a JavaScript library for building user interfaces. It lets you create reusable building blocks called components. These components make code cleaner and easier to manage. React uses a clever technique to update only the parts of the screen that change, improving app performance. With its focus on components and clear syntax, React is a popular tool for modern web development.
+        React is a JavaScript library for building user interfaces. It lets you create reusable building blocks called components. These components make code cleaner and easier to manage. React uses a clever technique to update only the parts of the screen that change, improving app performance. With its focus on components and clear syntax, React is a popular tool for modern web development.
+        </p>
+        <ol>
+          <li>Catherine Arcamo</li>
+          <li>Johnny Doe</li>
+        </ol>
+    </div>
+  )
+}
+
+
+export default App;
